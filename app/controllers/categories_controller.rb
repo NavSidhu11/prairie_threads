39337d8b446ff58a371d2e9929dc4ac57849b731
NavSidhu11 <nsidhu@rrc.ca>
2026-04-01 13:03:13 -0500

    Implemented category-based product navigation and display

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @products = @category.products.page(params[:page]).per(12)
+
+    @products = @category.products.page(params[:page]).per(10)
   end
 end
