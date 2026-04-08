1afb40c5d13b445b66181aa7aa35ad723d2a8edb
NavSidhu11 <nsidhu@rrc.ca>
2026-04-08 08:32:58 -0500

    Added admin order management panel with status update functionality

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,10 +1,27 @@
 ActiveAdmin.register Order do
-  config.filters = false
+
   permit_params :status
- form do |f|
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :status
+    column :subtotal
+    column :tax_total
+    column :grand_total
+    column :created_at
+    actions
+  end
+
+  filter :status
+  filter :created_at
+
+  form do |f|
     f.inputs do
-      f.input :status, as: :select, collection: ['new', 'paid', 'shipped']
+      f.input :status, as: :select, collection: ["new", "paid", "shipped"]
     end
     f.actions
   end
+
 end
