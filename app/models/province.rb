e063246088131cf83ee7f667b7b75e6060bc10a1
NavSidhu11 <nsidhu@rrc.ca>
2026-04-08 08:59:25 -0500

    Update all

--- app/models/province.rb
+++ app/models/province.rb
@@ -4,4 +4,12 @@ class Province < ApplicationRecord
 
   validates :name, :abbreviation, presence: true
   validates :gst_rate, :pst_rate, :hst_rate, presence: true, numericality: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "name", "abbreviation", "gst_rate", "pst_rate", "hst_rate", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["addresses", "users"]
+  end
 end
