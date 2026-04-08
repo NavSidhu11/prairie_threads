c9be1ad5f230fbd89435e208c6c47d3821aa9819
NavSidhu11 <nsidhu@rrc.ca>
2026-04-08 08:52:21 -0500

    Added Ransack allowlist for Address model to fix ActiveAdmin filtering

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,21 @@ class Address < ApplicationRecord
   has_many :orders
 
   validates :street, :city, :postal_code, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+  [
+    "id",
+    "street",
+    "city",
+    "postal_code",
+    "province_id",
+    "user_id",
+    "created_at",
+    "updated_at"
+  ]
+end
+
+def self.ransackable_associations(auth_object = nil)
+  ["user", "province"]
+end
 end
