aa88e3f36aeab47e76eaadcc23bd60bce013f76b
NavSidhu11 <nsidhu@rrc.ca>
2026-04-08 08:42:10 -0500

    Added admin interface for managing province tax rates

--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -1,4 +1,27 @@
 ActiveAdmin.register Province do
-  config.filters = false
+
   permit_params :name, :abbreviation, :gst_rate, :pst_rate, :hst_rate
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :abbreviation
+    column :gst_rate
+    column :pst_rate
+    column :hst_rate
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :abbreviation
+      f.input :gst_rate
+      f.input :pst_rate
+      f.input :hst_rate
+    end
+    f.actions
+  end
+
 end
