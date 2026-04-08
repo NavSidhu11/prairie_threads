af6a00fb077485759def57680f05e81615b062cc
NavSidhu11 <nsidhu@rrc.ca>
2026-04-08 08:53:06 -0500

    Added Ransack allowlist for User model to fix ActiveAdmin filtering

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,21 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+  [
+    "id",
+    "email",
+    "first_name",
+    "last_name",
+    "phone",
+    "province_id",
+    "created_at",
+    "updated_at"
+  ]
+end
+
+def self.ransackable_associations(auth_object = nil)
+  ["province", "addresses", "orders"]
+end
 end
